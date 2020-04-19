6b205c74aa999227ae9302838adf45bd8c610463
Alexey Bystrov <alexeybystrov@me.com>
2020-04-19 22:23:33 +0300

    create gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import program from 'commander';
+import genDiff from '../src/index.js';
 
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
-  .arguments('<firstConfig>, <secondConfig>')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((first, second) => console.log(genDiff(first, second)))
   .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-werwef
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+
+const genDiff = (before, after) => {
+  // const firstPath = fs.readFileSync(path.resolve(before));
+  // const secondPath = fs.readFileSync(path.resolve(after));
+  const firstObject = JSON.parse(fs.readFileSync(path.resolve(before)));
+  const secondObject = JSON.parse(fs.readFileSync(path.resolve(after)));
+  const unitedKeys = _.uniq([...Object.keys(firstObject), ...Object.keys(secondObject)]);
+
+  const diff = unitedKeys.reduce((acc, key) => {
+    if (!_.has(firstObject, key)) {
+      acc.push(`  + ${key}: ${secondObject[key]}\n`);
+      return acc;
+    }
+
+    if (!_.has(secondObject, key)) {
+      acc.push(`  - ${key}: ${firstObject[key]}\n`);
+      return acc;
+    }
+
+    if (firstObject[key] === secondObject[key]) {
+      acc.push(`    ${key}: ${firstObject[key]}\n`);
+      return acc;
+    }
+
+    acc.push(`  - ${key}: ${firstObject[key]}\n`);
+    acc.push(`  + ${key}: ${secondObject[key]}\n`);
+    return acc;
+  }, []);
+
+  return `{\n${diff.join('')}}`;
+};
+
+export default genDiff;
+
+/* $ gendiff before.json after.json
+{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+  - follow: false
+} */
