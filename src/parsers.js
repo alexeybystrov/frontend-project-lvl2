fa328ef7fe8a8e033aa0bfab2542eccd99c06ab7
Alexey Bystrov <alexeybystrov@me.com>
2020-05-13 19:33:29 +0300

    refactor parsers

--- src/parsers.js
+++ src/parsers.js
@@ -6,37 +6,36 @@ import _ from 'lodash';
 
 const readFile = (dir) => fs.readFileSync(path.resolve(dir), 'utf8');
 
+const iniParser = (content) => {
+  const data = ini.parse(content);
+
+  const iter = (node) => Object.entries(node).reduce((acc, [key, value]) => {
+    if (_.isObjectLike(value)) {
+      acc[key] = iter(value);
+    } else if (typeof (value) === 'boolean') {
+      acc[key] = value;
+    } else if (!Number.isNaN(Number(value))) {
+      acc[key] = Number(value);
+    } else {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
+
+  return iter(data);
+};
+
 const parser = (pathToFile) => {
-  const fileContent = readFile(pathToFile);
-  const iniParser = (content) => {
-    const data = ini.parse(content);
-
-    const iter = (node) => Object.entries(node).reduce((acc, [key, value]) => {
-      if (_.isObjectLike(value)) {
-        acc[key] = iter(value);
-      } else if (typeof (value) === 'boolean') {
-        acc[key] = value;
-      } else if (!Number.isNaN(Number(value))) {
-        acc[key] = Number(value);
-      } else {
-        acc[key] = value;
-      }
-      return acc;
-    }, {});
-
-    return iter(data);
-  };
-
   let result;
   switch (path.extname(path.resolve(pathToFile))) {
     case '.yml':
-      result = yaml.safeLoad(fileContent);
+      result = yaml.safeLoad(readFile(pathToFile));
       break;
     case '.ini':
-      result = iniParser(fileContent);
+      result = iniParser(readFile(pathToFile));
       break;
     case '.json':
-      result = JSON.parse(fileContent);
+      result = JSON.parse(readFile(pathToFile));
       break;
     default:
         // do nothing;
